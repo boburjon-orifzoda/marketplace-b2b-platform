6cea2cab8ac460eecdf5df6698d6e7886331a9f6
Bob <boburjon.orifzoda@icloud.com>
2026-04-10 05:25:02 -0700

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -6,6 +6,8 @@ from controllers.delivery_controller import delivery_blueprint
 from controllers.suppliers_controller import suppliers_blueprint
 from controllers.categories_controller import categories_blueprint
 from controllers.order_items_controller import order_items_blueprint
+from controllers.auth_controller import auth_blueprint
+from controllers.reporting_controller import reporting_blueprint
 
 app = Flask(__name__)
 
@@ -16,6 +18,8 @@ app.register_blueprint(delivery_blueprint, url_prefix="/api/delivery")
 app.register_blueprint(suppliers_blueprint, url_prefix="/api/suppliers")
 app.register_blueprint(categories_blueprint, url_prefix="/api/categories")
 app.register_blueprint(order_items_blueprint, url_prefix="/api/order-items")
+app.register_blueprint(auth_blueprint, url_prefix="/api/auth")
+app.register_blueprint(reporting_blueprint, url_prefix="/api/reporting")
 
 if __name__ == "__main__":
     app.run(debug=True)
